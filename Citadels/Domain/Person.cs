f16e410bedd21c9330453b1156b6f3e355b9bacd
AlexL33t <alexander_belyaev95@mail.ru>
2016-06-06 00:42:25 +0500

    Update Person.cs

--- Citadels/Domain/Person.cs
+++ Citadels/Domain/Person.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,5 +20,27 @@ namespace Citadels.Domain
          * зодчий
          * военный(красный) '2' способности 
          */
+        #region equals, gethashcode
+        protected bool Equals(Person other)
+        {
+            return Rank == other.Rank && (int)Color == (int)other.Color;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Choice)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return Rank ^ (int)Color + 123;
+            }
+        }
+        #endregion
     }
 }
