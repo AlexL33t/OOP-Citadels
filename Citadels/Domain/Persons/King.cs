69159505dd584bf5a3d5de857870437958692721
AlexL33t <alexander_belyaev95@mail.ru>
2016-06-06 00:39:09 +0500

    Update King.cs

--- Citadels/Domain/Persons/King.cs
+++ Citadels/Domain/Persons/King.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +11,7 @@ namespace Citadels.Domain
         public King()
         {
             Rank = 4;
+            Color = QuarterColor.Yellow;
         }
 
         void Сoronation(Player king, GameField field)
