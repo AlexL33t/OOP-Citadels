39c60aa2b6215914a9121fed7b8f51f1f5b2879b
AlexL33t <alexander_belyaev95@mail.ru>
2016-06-06 00:43:59 +0500

    Update Player.cs

--- Citadels/Domain/Player.cs
+++ Citadels/Domain/Player.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +8,38 @@ namespace Citadels.Domain
 {
     public class Player
     {
-        public string Name;
+        public string Name { get; private set; }
+        public string Id { get; private set; }
+
+        public Player(string id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+
+        #region entity semantics
+        protected bool Equals(Player other)
+        {
+            return string.Equals(Id, other.Id);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Player)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Player: {0} {1}", Name, Id);
+        }
+        #endregion
     }
 }
