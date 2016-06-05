6bf5e5f2eb52989135318e8b10b90bfba7ae10e4
AlexL33t <alexander_belyaev95@mail.ru>
2016-06-06 00:46:38 +0500

    Add files via upload

---
+++ Citadels/Domain/State.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Citadels.Domain
+{
+    public enum State
+    {
+        InitRound,
+        PlayingRound 
+    }
+}
