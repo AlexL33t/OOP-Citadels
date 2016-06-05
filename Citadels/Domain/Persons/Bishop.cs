18b5d43b5b41403116c460e42ea88ef77c7e3ee8
AlexL33t <alexander_belyaev95@mail.ru>
2016-06-06 00:41:06 +0500

    Add files via upload

---
+++ Citadels/Domain/Persons/Bishop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Citadels.Domain
+{
+    public class Bishop : Person
+    {
+        public Bishop()
+        {
+            Rank = 5;
+            Color = QuarterColor.Blue;
+        }
+    }
+}
