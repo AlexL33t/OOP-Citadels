873c524f8d02b046050ed4e01e5cdd92a6d98b52
AlekseyAnt <aleksei193@mail.ru>
2016-06-09 00:04:01 +0500

    Update Assassin.cs

--- Citadels/Domain/Persons/Assassin.cs
+++ Citadels/Domain/Persons/Assassin.cs
@@ -1,21 +1,39 @@
-﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Citadels.Domain
 {
-    public class Assassin : Person
+    class Assassin : Person
     {
+        class KillPerson : Act
+        {
+            public KillPerson(Player player, Person person, GameField field) :
+                base(player, person, field) { }
+
+            public override List<object> GetParam()
+            {
+                return new List<object>(field.Persons);
+            }
+
+            public override void Do(int[] choice, List<object> answ, Flags flags)
+            {
+                field.KillPerson((Person)answ[choice[0]]);
+                flags.AddActionDone = true;
+            }
+
+            public override InfoAct Info
+            {
+                get { return new InfoAct() { Name = "" }; }
+            }
+        }
+
         public Assassin()
         {
             Rank = 1;
         }
 
-        public void ActionPerson(Choice choice, GameField field)
+        public override List<Act> GetPossibleActons(Player player, Person person, Flags flags, GameField field)
         {
-            field.KillPerson(choice.Person);
+            return new List<Act>(){new KillPerson(player, person, field)};
         }
     }
 }
