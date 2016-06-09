4f3381a7b676682fa12ecda677ad9311a87c3f1f
AlexL33t <alexander_belyaev95@mail.ru>
2016-06-09 15:27:47 +0500

    Update ChooseQuarter.cs

--- Citadels/Domain/Action/ChooseQuarter.cs
+++ Citadels/Domain/Action/ChooseQuarter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -29,7 +29,7 @@ namespace Citadels.Domain
 
         public override InfoAct Info
         {
-            get { return new InfoAct() { Name = "" }; }
+            get { return new InfoAct() { Name = "Взять квартал" }; }
         }
     }
 }
